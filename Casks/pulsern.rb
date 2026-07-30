570d30f4b5ed350c9c884176776205b82007fc5c
Mahendra Bhama <maahibhama@gmail.com>
2026-07-30 23:19:57 +0530

    chore(release): prepare v0.1.1

--- Casks/pulsern.rb
+++ Casks/pulsern.rb
@@ -1,5 +1,5 @@
 cask "pulsern" do
-  version "0.1.0"
+  version "0.1.1"
   sha256 :no_check
 
   url "https://github.com/maahibhama/PulseRN/releases/download/v#{version}/PulseRN-#{version}-mac-universal.dmg"
