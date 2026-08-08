5dfba94c8c851f5afcf7dc7818264a94875450b1
Mahendra Bhama <maahibhama@gmail.com>
2026-08-08 16:20:57 +0530

    fix(cli): use GitHub artifact checksum

--- Formula/pulsern-cli.rb
+++ Formula/pulsern-cli.rb
@@ -2,7 +2,7 @@ class PulsernCli < Formula
   desc "Run the PulseRN React Native debugger in a local web browser"
   homepage "https://github.com/maahibhama/PulseRN"
   url "https://github.com/maahibhama/PulseRN/releases/download/cli-v1.0.4/pulsern-1.0.4.tgz"
-  sha256 "12a59a34ea5106f3b1738feb5034e8d9d919a90ea132424732da17f2cb26d314"
+  sha256 "8ef0ae6f5974332a0ee0cfd284f38240aae42feeadb6f7c35d8ed9972ca19fbb"
   license "MIT"
 
   depends_on "node"
