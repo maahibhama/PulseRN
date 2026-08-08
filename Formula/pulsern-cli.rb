5c7e68aa08126f919700819a7fa6f50a0fc9f69d
Mahendra Bhama <maahibhama@gmail.com>
2026-08-08 16:16:06 +0530

    fix(cli): make clean CI release reproducible

--- Formula/pulsern-cli.rb
+++ Formula/pulsern-cli.rb
@@ -2,7 +2,7 @@ class PulsernCli < Formula
   desc "Run the PulseRN React Native debugger in a local web browser"
   homepage "https://github.com/maahibhama/PulseRN"
   url "https://github.com/maahibhama/PulseRN/releases/download/cli-v1.0.4/pulsern-1.0.4.tgz"
-  sha256 "d3d5fac7eb105553a09066cb3eda409d44d62e3dfeb19b77c959ba68fbbc09ac"
+  sha256 "12a59a34ea5106f3b1738feb5034e8d9d919a90ea132424732da17f2cb26d314"
   license "MIT"
 
   depends_on "node"
